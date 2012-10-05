a2f70ba99ad3c2cd43d3639b804d512ca2eeeb37
Gray Gwizdz <gray.gwizdz@gmail.com>
2012-10-05 11:28:38 -0400

    Sphinx tools updated based on template

---
+++ cpg_islands/metadata.py
@@ -0,0 +1,16 @@
+""":mod:`cpg_islands.metadata` --- Project metadata
+
+Information describing the project.
+"""
+
+title = 'cpg_islands'
+nice_title = "CpG Island Finder"
+nice_title_no_spaces = nice_title.replace(' ', '')
+version = '0.1'
+description = 'Finding CpG islands in genetic sequences.'
+authors = ['Gray Gwizdz', 'Seank Fisk']
+authors_string = ', '.join(authors)
+emails = ['gwizdzg@mail.gvsu.edu', 'fiskse@mail.gvsu.edu']
+license = 'ISC'
+copyright = '2012 ' + authors_string
+url = 'https://github.com/seanfisk/cpg-islands'
