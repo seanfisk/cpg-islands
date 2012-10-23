bf6371844b145233be6d73e258a4cdc7c49aff18
Sean Fisk <sean@seanfisk.com>
2012-10-22 21:58:58 -0400

    Update environment file. Not very useful as most things were moved into shovel.py.

--- environment.sh
+++ environment.sh
@@ -3,11 +3,6 @@
 #     source environment.sh
 #
 
-export PYTHONPATH="$PWD"
-
 # fun aliases
 
 alias whatamidoinghere='git status'
-# generate tags for Emacs
-alias gentags='ctags -eR cpg_islands tests setup.py test.py'
-alias t='python test.py'
