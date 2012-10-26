b85d71b54737cfec219dac6f48eea04875f58b71
Gray Gwizdz <gray.gwizdz@gmail.com>
2012-10-26 12:51:42 -0400

    Gui template set up

--- cpg_islands/qt/views.py
+++ cpg_islands/qt/views.py
@@ -4,10 +4,122 @@
 from PySide import QtGui
 
 from cpg_islands.views import BaseApplicationView
+from cpg_islands.utils import Event
 
 
 class ApplicationView(QtGui.QMainWindow, BaseApplicationView):
+    """Primary application view."""
+    submitted = Event()
+
+    def __init__(self, parent=None):
+        """Construct a main view.
+
+            :param parent: widget parent
+            :type parent: :class:`QtGui.QWidget`
+            """
+        super(ApplicationView, self).__init__(parent)
+
+        self.setCentralWidget(QtGui.QWidget(self))
+
+        # Menu
+        self.menu_bar = QtGui.QMenuBar()
+        self.file_menu = self.menu_bar.addMenu('&File')
+        self.quit_action = self.file_menu.addAction('&Quit')
+        self.quit_action.triggered.connect(self.close)
+        self.help_menu = self.menu_bar.addMenu('&Help')
+        self.about_action = self.help_menu.addAction('&About')
+        self.about_action.triggered.connect(self.about)
+        self.setMenuBar(self.menu_bar)
+
+        # Layout
+        self.layout = QtGui.QFormLayout(self.centralWidget())
+        self.sequence_input = QtGui.QPlainTextEdit(self.centralWidget())
+        self.layout.addRow('Sequence', self.sequence_input)
+        self.ratio_input = QtGui.QLineEdit(self.centralWidget())
+        self.layout.addRow('GC Ratio:', self.ratio_input)
+        self.island_input = QtGui.QLineEdit(self.centralWidget())
+        self.layout.addRow('Island Size:', self.island_input)
+        self.result_output = QtGui.QPlainTextEdit(self.centralWidget())
+        self.result_output.setReadOnly(True)
+        self.layout.addRow('Result', self.result_output)
+        self.submit_button = QtGui.QPushButton('Find Islands',
+                                               self.centralWidget())
+        self.submit_button.clicked.connect(self._submit_clicked)
+        self.layout.addRow(self.submit_button)
+
     def start(self):
         """Show and raise the window."""
         self.show()
         self.raise_()
+
+    def about(self):
+        """Create and show the about dialog."""
+        AboutDialog(self).exec_()
+
+    def get_sequence(self):
+        """Return the widget's entered text.
+
+            :return: the text
+            :rtype: :class:`str`
+            """
+        return self.text_input.toPlainText()
+
+    def get_gc(self):
+        """Return the widget's entered GC ratio.
+
+            :return: the key
+            :rtype: :class:`str`
+            """
+        return self.island_input.text()
+
+    def get_island(self):
+        """Return the widget's entered island size.
+
+            :return: the key
+            :rtype: :class:`str`
+            """
+        return self.island_input.text()
+
+    def set_locations(self, locations):
+        """Set encoded text result.
+
+            :param result: the encoded text
+            :type locations: :class:`list` of :class:`tuple`
+            """
+    #self.result_output.setPlainText(result)
+
+    def show_error(self, message):
+        """Show the user an error dialog.
+
+            :param message: error message
+            :type message: :class:`str`
+            """
+        error_dialog = QtGui.QErrorMessage(self)
+        error_dialog.showMessage(message)
+
+    def _submit_clicked(self):
+        self.submitted(self.get_sequence(), self.get_island(), self.get_gc())
+
+
+class AboutDialog(QtGui.QDialog):
+    """Shows information about the program."""
+    def __init__(self, parent=None):
+        """Construct the dialog.
+
+            :param parent: the widget's parent
+            :type parent: :class:`QtGui.QWidget`
+            """
+        super(AboutDialog, self).__init__(parent)
+        self.setWindowTitle('About ' + metadata.nice_title)
+        self.layout = QtGui.QVBoxLayout(self)
+        self.title_label = QtGui.QLabel(metadata.nice_title, self)
+        self.layout.addWidget(self.title_label)
+        self.version_label = QtGui.QLabel('Version ' + metadata.version, self)
+        self.layout.addWidget(self.version_label)
+        self.copyright_label = QtGui.QLabel('Copyright (C) ' +
+                                            metadata.copyright, self)
+        self.layout.addWidget(self.copyright_label)
+        self.url_label = QtGui.QLabel(
+            '<a href="{0}">{0}</a>'.format(metadata.url), self)
+        self.url_label.setOpenExternalLinks(True)
+        self.layout.addWidget(self.url_label)
