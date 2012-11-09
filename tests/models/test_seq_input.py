90ec5fdc9ef2bdb099753067bdcf51eb1a98b8dd
Sean Fisk <sean@seanfisk.com>
2012-11-09 10:53:36 -0500

    Add another test for SeqInputModel.

--- tests/models/test_seq_input.py
+++ tests/models/test_seq_input.py
@@ -1,5 +1,5 @@
 import pytest
-from mock import create_autospec, MagicMock, call
+from mock import create_autospec, MagicMock, call, sentinel
 
 from cpg_islands.models import SeqInputModel, MetaResultsModel
 from tests.helpers import fixture_file, read_fixture_file
@@ -17,6 +17,7 @@ class TestSeqInputModel:
         model.island_definition_defaults_set.append(callback)
         model.set_island_definition_defaults()
         assert callback.mock_calls == [call(200, 0.6)]
+        assert model.results_model.mock_calls == []
 
     class TestLoadFile:
         def test_load_jx500709_1(self, model):
@@ -25,6 +26,7 @@ class TestSeqInputModel:
             model.load_file(fixture_file('JX500709.1.gb'))
             assert (file_loaded_callback.mock_calls ==
                     [call(read_fixture_file('JX500709.1.flattened'))])
+            assert model.results_model.mock_calls == []
 
         def test_load_genbank_no_dna(self, model):
             error_raised_callback = MagicMock()
@@ -32,6 +34,7 @@ class TestSeqInputModel:
             model.load_file(fixture_file('JX500709.1.no-dna.gb'))
             assert (error_raised_callback.mock_calls ==
                     [call('Premature end of line during sequence data')])
+            assert model.results_model.mock_calls == []
 
         def test_load_genbank_empty(self, model):
             error_raised_callback = MagicMock()
@@ -39,6 +42,7 @@ class TestSeqInputModel:
             model.load_file(fixture_file('empty.gb'))
             assert (error_raised_callback.mock_calls ==
                     [call('No records found in handle')])
+            assert model.results_model.mock_calls == []
 
         def test_load_genbank_two_records(self, model):
             error_raised_callback = MagicMock()
@@ -46,3 +50,13 @@ class TestSeqInputModel:
             model.load_file(fixture_file('U49845.1-and-JX500709.1.gb'))
             assert (error_raised_callback.mock_calls ==
                     [call('More than one record found in handle')])
+            assert model.results_model.mock_calls == []
+
+    def test_annotate_cpg_islands(self, model):
+        model.annotate_cpg_islands(
+            sentinel.seq, sentinel.island_size, sentinel.min_gc_ratio)
+        assert (model.results_model.mock_calls ==
+                [call.annotate_cpg_islands(
+                    sentinel.seq,
+                    sentinel.island_size,
+                    sentinel.min_gc_ratio)])
